a3c919df3b659eb61d3b64f9989474df26ba6c01
Tom Doder <tdoder89@gmail.com>
2022-12-01 19:19:09 -0600

    add book answers to line 15

--- src/book.js
+++ src/book.js
@@ -1,11 +1,22 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return `The ${title}`
+  }
+function buildMainCharacter(name, age, pronouns){
+  var character = {
+    name: name,
+    age: age,
+    pronouns: pronouns
+  }
+  return character
 }
-
+function saveReview(reviews) {
+  var newReview = []
+  return newReview
+}
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
+  buildMainCharacter,
+  saveReview,
   // calculatePageCount,
   // writeBook,
   // editBook
