40d08ec42de90cc214d2f030c28fe594c1fa716c
Tom Doder <tdoder89@gmail.com>
2022-12-02 21:01:43 -0600

    add changes to bookjs

--- src/book.js
+++ src/book.js
@@ -2,26 +2,43 @@ function createTitle(title) {
   return `The ${title}`
   }
 function buildMainCharacter(name, age, pronouns){
-  var character = {
+  var bookCharacter = {
     name: name,
     age: age,
     pronouns: pronouns
   }
-  return character
+  return bookCharacter
 }
-function saveReview(addReviews, array) {
- if (array.includes(addReviews) === false) {
-    return array.push(addReviews)
+function saveReview(reviews, array) {
+ if (array.includes(reviews) === false) {
+    return array.push(reviews)
   // } else {
     // return array
   }
 }
+function calculatePageCount(title) {
+  var bookPageCount = title.length * 20
+  return bookPageCount
+}
+function writeBook(createTitle, bookCharacter, genre) {
+  var book = {
+    title: createTitle,
+    mainCharacter: bookCharacter,
+    pageCount: calculatePageCount(createTitle),
+    genre: genre
+  }
+  
+ return book
+}
+function editBook(writeBook) {
+  return writeBook.pageCount = writeBook.pageCount * 0.75
 
+}
 module.exports = {
   createTitle,
   buildMainCharacter,
   saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  calculatePageCount,
+  writeBook,
+  editBook
 }
