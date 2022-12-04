9bcdc029c917a6e9d3ab0f6ddd33d1be59fbcdf5
Tom Doder <tdoder89@gmail.com>
2022-12-04 14:52:57 -0600

    show work on checkoutBook

--- src/Library.js
+++ src/Library.js
@@ -9,9 +9,16 @@ function createLibrary(library) {
 function addBook(library, book) {
   library.shelves[book.genre].push(book)
 }
-
+function checkoutBook(library, book, shelves) { 
+  for (var i = 0; i < shelves.length; i++) {
+    if (shelves[i] === book) {
+    library.shelves[i].splice(i, 1)
+}
+}
+ return `You have now checked out ${book} from the ${library}`
+}
 module.exports = {
   createLibrary,
   addBook,
-  // checkoutBook
+  checkoutBook
 };
