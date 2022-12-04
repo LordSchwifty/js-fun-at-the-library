cc7282ca5a43c4cd5252efa4229d876b532ad637
Tom Doder <tdoder89@gmail.com>
2022-12-03 19:39:55 -0600

    add up to addBook on library

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,17 @@
-
+function createLibrary(library) {
+ return library = {
+  name: library,
+  shelves: { 
+    fantasy: [], fiction: [], nonFiction: []
+ }
+}
+}
+function addBook(library, book) {
+  library.shelves[book.genre].push(book)
+}
 
 module.exports = {
-  // createLibrary,
-  // addBook,
+  createLibrary,
+  addBook,
   // checkoutBook
 };
