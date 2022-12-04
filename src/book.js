2ecfc47fd5b3564204beeb46f8b25537cfff8fad
Tom Doder <tdoder89@gmail.com>
2022-12-04 10:59:02 -0600

    syntax cleanup

--- src/book.js
+++ src/book.js
@@ -1,33 +1,30 @@
 function createTitle(title) {
-  return `The ${title}`
-  }
+ return `The ${title}`
+}
 function buildMainCharacter(name, age, pronouns){
-  var bookCharacter = {
-    name: name,
-    age: age,
-    pronouns: pronouns
-  }
+var bookCharacter = {
+  name: name,
+  age: age,
+  pronouns: pronouns
+ }
   return bookCharacter
 }
 function saveReview(reviews, array) {
  if (array.includes(reviews) === false) {
     return array.push(reviews)
-  // } else {
-    // return array
-  }
+}
 }
 function calculatePageCount(title) {
   var bookPageCount = title.length * 20
   return bookPageCount
 }
 function writeBook(createTitle, bookCharacter, genre) {
-  var book = {
+ var book = {
     title: createTitle,
     mainCharacter: bookCharacter,
     pageCount: calculatePageCount(createTitle),
     genre: genre
   }
-  
  return book
 }
 function editBook(writeBook) {
